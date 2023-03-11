a96a85c8ba0156861c137c496b55b7d06e55805d
Zhanping <71991085+zhanpings510@users.noreply.github.com>
2023-03-10 23:10:22 -0600

    Add files via upload

---
+++ Davinci.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTAPITest.Models
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Choice
+    {
+        public string text { get; set; }
+        public int index { get; set; }
+        public object logprobs { get; set; }
+        public string finish_reason { get; set; }
+    }
+
+    public class Davinci
+    {
+        public string id { get; set; }
+        public string @object { get; set; }
+        public int created { get; set; }
+        public string model { get; set; }
+        public List<Choice> choices { get; set; }
+        public Usage usage { get; set; }
+    }
+
+    public class Usage
+    {
+        public int prompt_tokens { get; set; }
+        public int completion_tokens { get; set; }
+        public int total_tokens { get; set; }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft;
+
+
+/// <summary>
+/// This is first code by using ChatGPT API to get the result with a Model called "Davinci"
+/// March 6, 2023
+/// </summary>
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var client = new HttpClient();
+        var url = "https://api.openai.com/v1/completions";
+
+        // Create the request payload as a string
+        var payload = @"{
+            ""model"": ""text-davinci-003"",
+            ""prompt"": ""Say this is a test"",
+            ""max_tokens"": 7,
+            ""temperature"": 0
+        }";
+
+        // Create the HTTP request message
+        var request = new HttpRequestMessage(HttpMethod.Post, url);
+        request.Content = new StringContent(payload, System.Text.Encoding.UTF8, "application/json");
+
+        // Set the Authorization header
+        request.Headers.Add("Authorization", "Bearer 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");
+
+        // Send the request and get the response
+        var response = await client.SendAsync(request);
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        Console.WriteLine(responseString);
+        Console.ReadLine(); 
+    }
+}
+
+
+//This code uses the HttpClient class to create a POST request to the specified URL with the JSON payload. It sets the Content-Type and Authorization headers to match the Curl command. Finally, it sends the request and retrieves the response as a string.
+//Note that you'll need to replace the bearer token in the Authorization header with your own token, as the one in the example is just a placeholder.
+
+
+
+
+
